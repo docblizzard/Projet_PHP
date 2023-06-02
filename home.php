eef3d221e7f617a72b47aed0cf607e22c5553f6a
Docblizzard <stephanelwin@hotmail.fr>
2023-06-02 17:38:46 +0200

    try on one img for div

--- home.php
+++ home.php
@@ -5,7 +5,6 @@
   <link rel="stylesheet" type ="text/css" href="style.css">
 </head>
 <body>
-
   <!-- Navigation bar -->
   <div class="navbar">
     <a href="#">Home</a>
@@ -23,22 +22,32 @@
       array("images/lg_shifter.jpeg", "Logitech Shifter"),
       array("images/tm_ring.jpeg", "Thrustmaster Quick Release"),
       array("images/tm_shifter.jpeg", "Thrustmaster shifter")
-
       );
     
       foreach ($images as $image) {
         $src = $image[0];
         $alt = $image[1];
         echo '<div class="slide">';
-        echo '<img src="' . $src . '" alt="' . $alt;
+        echo '<img src="' . $src . '" alt="' . $alt . '">';
         echo '</div>';
       }
     ?>
+    <div id="previous" onclick="changeSlide(-1)"><</div>
+    <div id="next" onclick="changeSlide(1)">></div>
+  </div>
+  
+  <div class="navigation-button">
+      <span class="dot active" onclick="changeSlide(0)"></span>
+      <span class="dot" onclick="changeSlide(1)"></span>
+      <span class="dot" onclick="changeSlide(2)"></span>
+      <span class="dot" onclick="changeSlide(3)"></span>
+
   </div>
 
   <footer class="footer">
     <p>&copy; 2023 Sim Racing Homepage. All rights reserved.</p>
   </footer>
 
+  <script src="script.js"></script>
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,24 @@
+var imgs = document.querySelectorAll('.slideshow img');
+var dots = document.querySelectorAll('.dot');
+var currentImg = 0; // index of the first image 
+const interval = 4000; // duration(speed) of the slide
+
+function changeSlide(n) {
+  for (var i = 0; i < imgs.length; i++) {
+    imgs[i].style.opacity = 0;
+    dots[i].className = dots[i].className.replace('active', '');
+  }
+
+  currentImg = (currentImg + 1) % imgs.length; // update the index number
+
+  if (n != undefined) {
+      clearInterval(timer);
+      timer = setInterval(changeSlide, interval);
+      currentImg = n;
+  }
+
+  imgs[currentImg].style.opacity = 1;
+  dots[currentImg].className += ' active';
+}
+
+var timer = setInterval(changeSlide, interval);
